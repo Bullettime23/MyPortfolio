4d3fbc85adfead742fcf1c22c784e0ada5ee36b6
Bullettime23 <letsgofrontend@gmail.com>
2026-03-31 21:44:23 +0500

    Add: Ball blast project and video player

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { projects } from '../utility/projects';
 
 export interface Project {
   title: string;
@@ -10,27 +11,12 @@ export interface Project {
   githubLink: string; 
 }
 
-const arcadeProject: Project = {
-  title: 'Simple arcade',
-  description:
-    'A simple arcade as a learn project made with Unity. In this game you need to find the keys and avoid traps to reach the final castle and open its gates.',
-  images: [
-    'assets/png/Simple_arcade/img1.png',
-    'assets/png/Simple_arcade/img2.png',
-    'assets/png/Simple_arcade/img3.png',
-  ],
-  videos: [],
-  overview:
-    'A simple arcade as a learn project made with Unity. In this 3D game you need to find the keys and avoid traps to reach the final castle and open its gates. In this project, I learned how to create an interface and a pause menu, use scripts with game objects, and use colliders as triggers. I add simple animation for things like moving platforms, accelerators, jump springs, and doors.',
-  toolsUsed: ['Unity 3D'],
-  githubLink: 'https://github.com/Bullettime23/Simple-Arcade/tree/main',
-};
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProjectService {
-  public GetProjectById(id: string) {
-    return arcadeProject;
+  public GetProjectById(id: string): Project | null {
+    return projects[id] ?? null;
   }
 }
